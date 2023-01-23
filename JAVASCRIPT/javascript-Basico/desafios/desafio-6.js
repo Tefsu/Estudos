524db6b8120447b861342c9cc4886fa10af5d0f2
Teferson.L <tefersonluan@gmail.com>
2023-01-23 16:58:57 -0300

    desafio 6

---
+++ JAVASCRIPT/javascript-Basico/desafios/desafio-6.js
@@ -0,0 +1,59 @@
+
+var objetos = Array('Cadeira', 'Impressora', 'Garfo');
+
+function adicionarObjeto(valor) {
+    console.log(valor)
+    var objeto = document.getElementById('objeto').value;
+
+    if (objeto === '') {
+        alert('Informe um valor valido!');
+    } else {
+        if (objetos.indexOf(objeto) !== -1) {
+            alert('Objeto já adicionado');
+            document.getElementById('objeto').value = '';
+        } else {
+            objetos.push(objeto);
+            console.log(objetos);
+            document.getElementById('objeto').value = '';
+        }
+    }
+}
+
+function ordenarObjetos() {
+    objetos.sort();
+    console.log(objetos);
+}
+
+/*
+var objetos = Array('Cadeira', 'Impressora', 'Garfo')
+function adicionarObjetos() {
+    //recupera o valor inserido no campo id objeto
+    var objeto = document.getElementById('objeto').value
+
+    //caso possua algum valor preenchido entra no if
+    if (objeto != '') {
+        //verifica se o valor já não foi inserido anteriormente
+        //se sim, informa que valor já existe, se não insere novo valor
+        if (objetos.indexOf(objeto) !== -1) {
+            alert('Objeto já foi adicionado')
+        } else {
+            objetos.push(objeto)
+            console.log(objetos)
+
+            document.getElementById('objeto').value = ''
+        }
+
+    } else {
+        alert('Informe um objeto válido')
+
+    }
+}
+function ordenarObjetos() {
+    objetos.sort()
+    console.log(objetos)
+}
+*/
+/*
+function test(teste){
+    console.log(teste)
+}*/
